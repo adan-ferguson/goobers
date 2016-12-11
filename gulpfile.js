f656802836f16d41ae277e363b0028fa52fb7766
Adan Ferguson <adanferguson@gmail.com>
2016-12-10 17:13:05 -0700

    gulpfile prereqs

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,28 @@
 var gulp = require('gulp');
 var inject = require('gulp-inject');
+var series = require('stream-series');
 var publicDir = './public/';
 
 gulp.task('inject', function(){
     "use strict";
 
     var target = gulp.src(publicDir + 'index.html');
-    var sources = gulp.src([publicDir + '/**/*.js', publicDir + '/**/*.css'], {read: false});
 
-    return target.pipe(inject(sources, {relative: true})).pipe(gulp.dest(publicDir));
+    var prereqs = [];
+    var rest = [publicDir + '**/*.js', publicDir + '**/*.css'];
+
+    for(var i = 0; i < prereqs.length; i++){
+        rest.push('!' + prereqs[i]);
+    }
+
+    var prereqStream = gulp.src(prereqs, {read: false});
+    var restStream = gulp.src(rest, {read: false});
+
+    return target.pipe(
+        inject(series(prereqStream, restStream), {relative: true})
+    ).pipe(
+        gulp.dest(publicDir)
+    );
 });
 
 gulp.task('watch', function(){
